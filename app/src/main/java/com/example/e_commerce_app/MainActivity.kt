26a51fee1ef13db89d6b2ac9a06c391370f4e21e
Mohit <92150824+MKG26@users.noreply.github.com>
2023-12-24 02:04:38 +0530

    .

--- app/src/main/java/com/example/e_commerce_app/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.e_commerce_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.e_commerce_app.ui.theme.Ecommerce_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    frontPage()
                 }
             }
         }
@@ -30,17 +40,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun frontPage( modifier: Modifier = Modifier) {
+
+
+
+    Column {
+
+
+
+
+        Image(
+            painter = painterResource(id = R.drawable.undraw_shopping_app_flsj__1_),
+            contentDescription = null,
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ecommerce_AppTheme {
-        Greeting("Android")
     }
 }
