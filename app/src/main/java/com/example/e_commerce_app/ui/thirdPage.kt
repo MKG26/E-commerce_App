557f4d20de0c6ac11228aefa5ad480cd98345fcf
Mohit <92150824+MKG26@users.noreply.github.com>
2023-12-26 17:38:18 +0530

    .

--- app/src/main/java/com/example/e_commerce_app/ui/thirdPage.kt
+++ app/src/main/java/com/example/e_commerce_app/ui/thirdPage.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -118,6 +119,7 @@ fun GridtwoCard(grid: Gridtwo, modifier: Modifier=Modifier){
         Image(
             painter = painterResource(id = grid.imageResourceId),
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .height(280.dp)
                 .width(400.dp)
