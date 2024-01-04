dc298eabe075769ca83e8cd81f50a42026c5cb13
Mohit <92150824+MKG26@users.noreply.github.com>
2024-01-05 01:43:17 +0530

    .

--- app/src/main/java/com/example/e_commerce_app/EcoScreen.kt
+++ app/src/main/java/com/example/e_commerce_app/EcoScreen.kt
@@ -1,12 +1,19 @@
 package com.example.e_commerce_app
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.e_commerce_app.ui.E_commerceViewModel
+import com.example.e_commerce_app.ui.SecondPage
+import com.example.e_commerce_app.ui.frontPage
+import java.lang.reflect.Modifier
+import androidx.navigation.NavHost as NavHost1
 
 
 enum class EcoScreen(@StringRes val title: Int){
@@ -22,5 +29,25 @@ fun EcoApp(
 
     ){
 
+    NavHost(
+        navController = navController,
+        startDestination = EcoScreen.Start.name
+    ){
+        composable(route = EcoScreen.Start.name){
+            frontPage()
+        }
+
+        composable(route = EcoScreen.SignUp.name){
+
+            SecondPage()
+
+        }
+
+        composable(route = EcoScreen.Order.name){
+
+
+        }
+
+    }
 
 }
